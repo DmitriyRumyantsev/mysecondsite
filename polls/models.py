2c211d36479445858628a70d696b8f25e9b4b96d
DmitriyRumyantsev <Guiatrmaster1703@mail.ru>
2021-02-27 19:53:55 +0700

    new commit

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+#Модель для отработки вопроса
+
+class Question(models.Model):
+    text = models.CharField(max_length=500)
+    pubDate = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.text
+
+#модель для отработки ответа
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+    text = models.CharField(max_length=500)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    q_all = Question.objects.all()
+    res = "<ol>"
+    for q in q_all:
+        res+="<li>%s</li>" % q.text
+    res+="</ol>"
+
+    return HttpResponse(res)
 
 def meme(request):
     return HttpResponse("<img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.latimes.com%2Fpolitics%2Fla-na-pol-pepe-the-frog-hate-symbol-20161011-snap-htmlstory.html&psig=AOvVaw0eCIYdXXcAReJk6Y5ka6NL&ust=1614509429344000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCKpbvyie8CFQAAAAAdAAAAABAD'>")
+
+def detail(request, q_id):
+    res = "Question № %s." % q_id
+    return HttpResponse(res)
+
+def results(request, q_id):
+    res = "Result question № %s." % q_id
+    return HttpResponse(res)
+
+def vote(request, q_id):
+    res = "Vote for question № %s." % q_id
+    return HttpResponse(res)
