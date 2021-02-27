a6edb024f10cd4d6b63336c87e36f237bb37f901
DmitriyRumyantsev <Guiatrmaster1703@mail.ru>
2021-02-27 17:53:38 +0700

    ya ustal

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+def meme(request):
+    return HttpResponse("<img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.latimes.com%2Fpolitics%2Fla-na-pol-pepe-the-frog-hate-symbol-20161011-snap-htmlstory.html&psig=AOvVaw0eCIYdXXcAReJk6Y5ka6NL&ust=1614509429344000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCKpbvyie8CFQAAAAAdAAAAABAD'>")
