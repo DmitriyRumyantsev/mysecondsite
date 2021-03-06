b9f4405c3791922aaa446355cb6a9803ad4d6d34
DmitriyRumyantsev <Guiatrmaster1703@mail.ru>
2021-03-06 19:51:11 +0700

    sixmart

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceAdmin(admin.StackedInline):
+    fieldsets = [
+        ("Вариант ответа", {"fields" : ["text", "votes"]} ),
+    ]
+    
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Текст вопроса", {"fields" : ["text"]} ),
+        ("Дата вопроса", {"fields" : ["pubDate"]} ),
+    ]
+
+    inlines = [ChoiceAdmin]
+    list_filter = ["pubDate"]
+    search_fields = ["text"]
+
+
+
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -7,20 +7,22 @@ from .models import Question, Choice
 
 
 def index(request):
-    q_all = Question.objects.all()
-    res = "<ol>"
-    for q in q_all:
-        res+="<li>%s</li>" % q.text
-    res+="</ol>"
+    question = Question.objects.all()
 
-    return HttpResponse(res)
+    context = {
+        "questions" : question
+    }
+    return render(request, "polls/index.html", context)
 
 def meme(request):
     return HttpResponse("<img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.latimes.com%2Fpolitics%2Fla-na-pol-pepe-the-frog-hate-symbol-20161011-snap-htmlstory.html&psig=AOvVaw0eCIYdXXcAReJk6Y5ka6NL&ust=1614509429344000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCKpbvyie8CFQAAAAAdAAAAABAD'>")
 
 def detail(request, q_id):
-    res = "Question № %s." % q_id
-    return HttpResponse(res)
+    question = Question.objects.get(pk=q_id)
+    context = {
+        "question" : question
+    }
+    return render(request, "polls/detail.html", context)
 
 def results(request, q_id):
     res = "Result question № %s." % q_id
